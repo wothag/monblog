bfc098afcccc20dabb7b827e84ee7a4b94ee3693
wothag <d.r.c.hagnere@gmail.com>
2017-12-24 15:06:52 +0100

    Creation du framework etape 1

---
+++ App/Frontend/FrontendApplication.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drcha
+ * Date: 20/12/2017
+ * Time: 18:58
+ */
+
+class FrontendApplication
+{
+
+}
---
+++ App/Frontend/Modules/Chapters/Chapter.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drcha
+ * Date: 18/12/2017
+ * Time: 19:24
+ */
+
+
+class Chapter
+{
+	protected $id;
+	protected $pubid;
+	protected $num;
+	protected $title;
+	protected $content;
+	protected $date_created;
+	protected $date_modified;
+	protected $userid;
+	protected $extrait;
+
+	/**
+	 * Chapter constructor.
+	 * @param $valeurs
+	 */
+
+
+
+	public function __construct($valeurs=[])
+	{
+		if (!empty($valeurs))
+		{
+			$this->hydrate($valeurs);
+			Echo 'construction ok';
+		}
+		return($valeurs);
+	}
+
+	public function hydrate($donnees)
+	{
+		foreach ($donnees as $attribute => $value)
+		{
+			$method = 'set' . ucfirst($attribute);
+			if (is_callable([$this, $method]))
+			{
+				$this->$method($value);
+			}
+		}
+		echo 'ok';
+	}
+
+
+//list des setters à faire
+
+	public function setId($id)
+	{
+		$this->id = (int) $id;
+	}
+
+	public function setPubid($pubid)
+	{
+		$this->pubid = (int) $pubid;
+	}
+
+	public function setNum($num)
+	{
+		$this->num = (int) $num;
+	}
+
+	public function setTitle($title)
+	{
+		if(!is_string($title))
+			{
+			echo'Veuillez entrer un titre valide svp !';
+			}
+		else
+			{
+			$this->title = $title;
+			}
+	}
+
+	public function setContent($content)
+	{
+		$this->$content = $content;
+	}
+
+
+	//liste des getters
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getNum()
+	{
+		return $this->$this->num;
+	}
+
+	public function pubId()
+	{
+		return $this->$this->pubId;
+	}
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	public function getDate_created()
+	{
+		return $this->date_created;
+	}
+
+	public function getDatemodified()
+	{
+		return $this->date_modified;
+	}
+
+}
